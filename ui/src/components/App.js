b750054f8bb29b1b0581b6d51a3d74317d2ed8e2
Daniel Lucas <daniel@ddluc.com>
2020-01-14 14:08:53 -0800

    Setup layout css grid

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/App.css'
+
+import Calendar from './Calendar.js';
+import Sidebar from './Sidebar.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Good Morning</h1>
+    <div className="app">
+      <Sidebar />
+      <Calendar />
+      <Footer />
     </div>
   );
 }
---
+++ ui/src/components/Calendar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Calendar() {
+  return (
+    <div className="calendar">
+        <p>Calendar</p>
+    </div>
+  );
+}
+
+export default Calendar;

3bfae7c009c1380742492ef34c215fe022b1d593
Daniel Lucas <daniel@ddluc.com>
2020-01-16 21:02:07 -0800

    Add comments to utility service

--- ui/src/services/UtilityService.js
+++ ui/src/services/UtilityService.js
@@ -1,5 +1,14 @@
-/** Utility Methods **/
+/**
+ * Utility Methods
+ * (i.e) Selectors
+ **/
 
+/**
+ * getMonthsInSeason
+ * @description gets the months for the active season
+ * @param <Array> months — array of months
+ * @param <Number> season — the id of the active season (1,2,3,4)
+ */
 export const getMonthsInSeason = (months, season) => {
   let monthsInSeason = [];
   months.forEach((month) => {
@@ -10,27 +19,69 @@ export const getMonthsInSeason = (months, season) => {
   return monthsInSeason;
 };
 
+/**
+ * getVegetablesInSeason
+ * @description gets the vegetables for the active season
+ * @param <Array> vegetables - array of vegetable objects
+ * @param <Array> months — array of months
+ * @param <Number> season — the id of the active season (1,2,3,4)
+ */
 export const getVegetablesInSeason = (vegetables, months, season) => {
   let vegetablesInSeason = [];
   const monthsInSeason = getMonthsInSeason(months, season);
   const monthKeys = monthsInSeason.map((month) => month.key);
   vegetables.forEach((vegetable) => {
-    let mPosCount = 0;
+    let inSeason = false;
     vegetable.mPos.forEach((mPos) => {
       if (monthKeys.includes(mPos.month)){
         if (mPos.status !== 'none') {
-          mPosCount = mPosCount + 1;
+          inSeason = true;
         }
       }
     });
-    if (mPosCount > 0) {
-      vegetablesInSeason.push({ ...vegetable, mPosCount: mPosCount})
+    if (inSeason) {
+      vegetablesInSeason.push({ ...vegetable})
     }
   });
   vegetablesInSeason.sort((a, b) => b.mPosCount - a.mPosCount);
   return vegetablesInSeason;
 };
 
+/**
+ * sortVegetablesByScore
+ * @description sorts the vegetables by custom algorithm to determine how in season it is
+ * @param <Array> vegetables - array of vegetable objects
+ * @param <Array> months — array of months
+ * @param <Number> season — the id of the active season (1,2,3,4)
+ */
+export const sortVegetables = (vegetables, months, season) => {
+  let vegetablesInSeason = [];
+  const monthsInSeason = getMonthsInSeason(months, season);
+  const monthKeys = monthsInSeason.map((month) => month.key);
+  vegetables.forEach((vegetable) => {
+    let inSeason = false;
+    vegetable.mPos.forEach((mPos) => {
+      if (monthKeys.includes(mPos.month)){
+        if (mPos.status !== 'none') {
+          inSeason = true;
+        }
+      }
+    });
+    if (inSeason) {
+      vegetablesInSeason.push({ ...vegetable})
+    }
+  });
+  vegetablesInSeason.sort((a, b) => b.mPosCount - a.mPosCount);
+  return vegetablesInSeason;
+};
+
+/**
+ * buildCellMap
+ * @description maps css grid cell indexes to month/segment
+ * @param <Array> months — array of months
+ * @param <Number> season — the id of the active season (1,2,3,4)
+ * @returns <Object> cellMap — a map of cell indexes
+ */
 export const buildCellMap = (months, season) => {
   let cellMap = {body: {}, header: {}};
   let bodyCellIndex = 1;
@@ -49,6 +100,12 @@ export const buildCellMap = (months, season) => {
   return cellMap;
 };
 
+
+/**
+ * buildSegmentMap
+ * @description builds a statuc segment array
+ * @returns <Array> segment
+ */
 export const buildSegmentMap = () => {
   const segments = [
     {id: 1, key: 'early', label: 'Early'},

bd1acbca812e31f3dacc6fba5f26160d97caae32
Daniel Lucas <daniel@ddluc.com>
2020-01-26 10:44:43 -0800

    Setup UI production build / configuration

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const Api = require('./api');
+const Api = require('./api/api');
 const cors = require('cors');
 
 const app = express();
 const port = 3300;
 
-app.use(cors()); 
+app.use(cors());
+app.use(express.static('public'));
+
 app.get('/api/market_data', Api.getMarketData);
 app.listen(port, () => console.log(`Harvest Calendar listening on port ${port}`))
---
+++ public/precache-manifest.77d01efe9c604354cea1dfd430eee27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7553ca32816e151387a91fe90b7e9f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5fc53e63d9e8f6f7988",
+    "url": "/static/css/main.dcd8080a.chunk.css"
+  },
+  {
+    "revision": "bf0292a136296bd9b560",
+    "url": "/static/js/2.f512b7b8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f512b7b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5fc53e63d9e8f6f7988",
+    "url": "/static/js/main.9fdceaa7.chunk.js"
+  },
+  {
+    "revision": "1d0676908afc7b541df6",
+    "url": "/static/js/runtime-main.29e4a0a0.js"
+  }
+]);

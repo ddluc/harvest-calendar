904e098ccd14fb382d2eaca926bb9fe8fba4099d
Daniel Lucas <daniel@ddluc.com>
2020-02-20 13:04:59 -0800

    Add enzyme + tests for App

--- ui/src/tests/components/App.test.js
+++ ui/src/tests/components/App.test.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from '../../components/App';
+import Calendar from '../../components/calendar/Calendar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import { shallow } from 'enzyme';
+
+
 
 describe('App', () => {
 
   it('should render the app', () => {
-    expect(true).toBeTruthy();
+    const wrapper = shallow(<App />);
+    expect(wrapper).toBeDefined();
+    expect(wrapper.find('.app')).toBeTruthy();
+    expect(wrapper.find('.acknowledgement')).toBeTruthy();
   })
-  
+
+  it('should render the calendar', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Calendar />)).toBeTruthy();
+  });
+
+  it('should render the calendar', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Sidebar />)).toBeTruthy();
+  });
+
+
 });
